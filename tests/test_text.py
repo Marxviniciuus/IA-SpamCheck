d1f134f36fb9242b7126278d92a0b579d0a22e8f
Rafael Souza <rafaeljosegomes25@gmail.com>
2024-03-07 11:25:33 -0300

    add test should_be_able_to_replace_punctuations

--- tests/test_text.py
+++ tests/test_text.py
@@ -12,6 +12,12 @@ def test_should_be_able_to_replace_special_chars():
     assert clean_text(text) == ' rafira '
 
 
+def test_should_be_able_to_replace_punctuations():
+    text = 'meu nome eh rafira! e o seu?'
+
+    assert clean_text(text) == 'meu nome eh rafira e o seu '
+
+
 def test_should_be_able_to_replace_large_blank_spaces():
     text = 'rafira    nao gosta     de        java'
 
