45b0f270d80755123818fce1f2126580a1b1b939
Rafael Souza <rafael.jose@ferreri.co>
2024-03-08 22:03:17 -0300

    update getTreatedData fn

--- treatment.py
+++ treatment.py
@@ -1,16 +1,16 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-import pandas as pd
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-from utils.text import clean_text
-
+from utils.preprocess import finalpreprocess
 
 def getTreatedData():
     df = pd.read_csv('spam.csv')
-    df['Message'] = df['Message'].apply(clean_text)
+    df['Message'] = df['Message'].apply(lambda x: finalpreprocess(x))
     df['Category'] = df['Category'].map({'ham': 0, 'spam': 1})
     tfidf = TfidfVectorizer(stop_words='english', max_features=5000)
     X = tfidf.fit_transform(df['Message'])
     y = df['Category']
-    
+
     return train_test_split(X, y, test_size=0.2, random_state=42)
+
