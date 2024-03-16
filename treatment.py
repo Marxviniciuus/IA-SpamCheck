1b76cf847b043313f3de6ddba90398c89a7c9560
Rafael Souza <rafael.jose@ferreri.co>
2024-03-16 20:02:24 -0300

    concat df2

--- treatment.py
+++ treatment.py
@@ -5,11 +5,19 @@ import pandas as pd
 from utils.preprocess import finalpreprocess
 
 def get_treated_data():
-    df = pd.read_csv('spam.csv')
+    df1 = pd.read_csv('spam.csv')
+    df1['Category'] = df1['Category'].map({'ham': 0, 'spam': 1})
+    df1 = df1.dropna(subset=['Message'])
+
+    df2 = pd.read_csv('spam2.csv', usecols=['Body', 'Label'])
+    df2 = df2.rename(columns={'Body': 'Message', 'Label': 'Category'})
+    df2 = df2.dropna(subset=['Message'])
+    
+    df = pd.concat([df1, df2], ignore_index=True)    
     df['Message'] = df['Message'].apply(lambda x: finalpreprocess(x))
-    df['Category'] = df['Category'].map({'ham': 0, 'spam': 1})
+
     tfidf = TfidfVectorizer(stop_words='english', max_features=5000)
     X = tfidf.fit_transform(df['Message'])
     y = df['Category']
-
+        
     return train_test_split(X, y, test_size=0.2, random_state=42)
